e5d88b744c38c6ef1a57b24fd90f0c42c54f2fc6
Sweepyoface <github@sweepy.pw>
2017-03-12 12:54:20 -0400

    New PyPI version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.5',
+    version='1.6',
 
     description='This utility provides an easy way of viewing total network traffic since the last boot.',
     long_description=long_description,
@@ -71,7 +71,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['netifaces'],
+    install_requires=['netifaces', 'uptime'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
